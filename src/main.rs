2a9ddeb2c3508ff6cf93f1cf8a245e2dd39202d3
Russ Goetz <russgoetz@gmail.com>
2022-05-27 17:06:33 -0400

    Fixed issue 1: Connect to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+#[macro_use]
+extern crate log;
+
+mod cli;
+mod net;
+
+macro_rules! may_fail {
+    ($expr:expr, $msg:expr) => ({
+        match $expr {
+            Ok(result) => result,
+            Err(err) => {
+                error!("{}: {}", $msg, err);
+                std::process::exit(1);
+            }
+        }  
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let cli = cli::Cli::parse();
+    let _server_sock = may_fail!(net::connect(&cli.ip_addr), "Failed to connect to the server");
 }
