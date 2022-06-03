22d89568afa1daa939463e120a58a919d5a378ea
Russ Goetz <russgoetz@gmail.com>
2022-06-03 06:11:21 -0400

    WIP

--- src/client.rs
+++ src/client.rs
@@ -1,15 +1,29 @@
-use std::{net::TcpStream, io::BufReader};
+use std::net::TcpStream;
+use std::io::{Read, BufReader};
 
 pub struct Client {
-    server_stream: TcpStream,
+    reader: BufReader<TcpStream>,
 }
 
 impl Client {
     pub fn new(server_stream: TcpStream) -> Self {
-        Client { server_stream }
+        Client { reader: BufReader::new(server_stream) }
     }
 
-    pub fn negotiate_options() {
-        let mut reader = BufReader::new(self)
+    pub fn log_rcvd_data(&mut self) {
+        let mut buf = [0u8; 100];
+        let num_bytes_rcvd = match self.reader.read(&mut buf) {
+            Ok(n) => n,
+            Err(e) => {
+                error!("Failed to log received data: {}", e);
+                return;
+            },
+        };
+        let mut hex_string = String::new();
+        for i in 0..num_bytes_rcvd {
+            let hex_digits = format!("{:02X}", buf[i]);
+            hex_string.push_str(&hex_digits);
+        }
+        info!("Received data: {}", hex_string);
     }
 }
